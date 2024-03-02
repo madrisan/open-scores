403f1a5a9e45cbc37c39bdc83eef312c32adb4ab
Davide Madrisan <d.madrisan@proton.me>
2024-03-02 23:16:14 +0100

    fix(translate.sh): text replacement fix
    Signed-off-by: Davide Madrisan <d.madrisan@proton.me>

--- lyinit/addons/translate.sh
+++ lyinit/addons/translate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # A very simple script for reformatting existing .ly code blocks
-# Copyright (c) 2023 Davide Madrisan <d.madrisan@proton.me>
+# Copyright (c) 2023-2024 Davide Madrisan <d.madrisan@proton.me>
 
 # Usage:
 #   translate.sh score.ly
@@ -26,32 +26,38 @@ while read line; do
    #echo "[debug]  | ${line/|*/}" \
    echo "  | $line" \
       | sed "# remove comments
-	     s@%.*@@;
-	     # remove final |
+             s@%.*@@;
+             # remove final |
              s/|[ ]*$//;
-	     # and extra spaces
-	     s@ \[@\[@g;
-	     s@ \]@\]@g;
-	     s@_\[@\[@g;
-	     s@  {@ {@g;
-	     s@ ~@~@g;
-	     s@[ ]\+@ @g;
-	     s@\^\[@\[@g;
-	     s@adjBeam[^ ]\+ @@g;
-	     s@adjTie[^ ]\+ @@g;
-	     # remove break and noBreak
-	     s@\\\\break@@g;s@\\\\noBreak@@g;
-	     s@times 2/3@tuplet 3/2@g;
-	     # common shortcuts
-	     s@\\\\sd@\\\\staffLower@g;
-	     s@\\\\su@\\\\staffUpper@g;
-             s@\\\\tb@\\\\tieNeutral@g;
-             s@\\\\td@\\\\tieDown@g;
-	     s@\\\\tu@\\\\tieUp@g;
-	     # a couple of useful substs with unfortunately side effects
-	     #s@\([a-g]\+\)s\([0-9,'=~\.]*\) @\1is\2 @g;
-	     #s@\([a-g]\+\)f\([0-9,'=~\.]*\) @\1es\2 @g;
-	     s@^ @  @;
-	     # remove final | (again)
-             s/|[ ]*$//;"
+             # and extra spaces
+             s@ \[@\[@g;
+             s@ \]@\]@g;
+             s@_\[@\[@g;
+             s@  {@ {@g;
+             s@ ~@~@g;
+             s@[ ]\+@ @g;
+             s@\^\[@\[@g;
+             s@adjBeam[^ ]\+ @@g;
+             s@adjTie[^ ]\+ @@g;
+             # remove break and noBreak
+             s@\\\\break@@g;s@\\\\noBreak@@g;
+             # remove barNumberCheck
+             s@\\\\barNumberCheck[ ]*[0-9]*@@;
+             s@times 2/3@tuplet 3/2@g;
+             # common shortcuts
+             s@\\\\sd @\\\\staffLower @g;
+             s@\\\\su @\\\\staffUpper @g;
+             s@\\\\tb @\\\\tieNeutral @g;
+             s@\\\\td @\\\\tieDown @g;
+             s@\\\\tu @\\\\tieUp @g;
+             # some notation substs with unfortunately side effects
+             # (so they must be manually enabled)
+             #s@\([a-g]\+\)s\([0-9,'=~\.]*\) @\1is\2 @g;
+             #s@\([a-g]\+\)f\([0-9,'=~\.]*\) @\1es\2 @g;
+             #s@ h\([0-9,'=~\.\\]*\) @ b\1 @g;s@ h @ b @g;s@ h\$@ b@;
+             s@^ @  @;
+             # remove final | (again)
+             s/|[ ]*$//;
+             # remove empty lines
+             /^[ \t]*$/d;"
 done < <(sed 's|\\|\\\\|g;' < "${1:-/dev/stdin}")
